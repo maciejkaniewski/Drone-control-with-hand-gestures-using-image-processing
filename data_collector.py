c80ca7baea468846576f97b161c07322b4fb4fb1
Maciej Kaniewski <maciejkaniewski1999@gmail.com>
2022-07-13 20:35:39 +0200

    Implement saving landmarks to the .csv file

--- data_collector.py
+++ data_collector.py
@@ -1,4 +1,5 @@
 from camera import Camera, cv
+from csv import writer
 
 
 def run_application():
@@ -9,18 +10,27 @@ def run_application():
     camera.init_hand_detection_model(False, 1, 0.8, 0.6)
     camera.start()
 
+    data = []
+
     while camera.capture.isOpened():
         camera.run()
         keyboard_key = cv.waitKey(5) & 0xFF
         if keyboard_key == ord("q"):
             break
-        elif keyboard_key == ord("m"):
+        elif keyboard_key == ord("m") and camera.multi_hand_landmarks is not None:
             print("Save mode")
-            print(
-                f'Index finger tip coordinates: (',
-                f'{camera.multi_hand_landmarks[0].landmark[20].x * camera.width}, '
-                f'{camera.multi_hand_landmarks[0].landmark[20].y * camera.height}) '
-            )
+
+            for i in camera.multi_hand_landmarks[0].landmark:
+                data.append(i.x*camera.width)
+                data.append(i.y*camera.height)
+
+            with open('data/CSVFILE.csv', 'a', newline='') as f_object:
+                writer_object = writer(f_object)
+                writer_object.writerow(data)
+                f_object.close()
+
+            data.clear()
+
     camera.clear()
 
 
