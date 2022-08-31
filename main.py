ea75eb83b8b4318b91b6bed122d2a3eb9e3a34bc
Maciej Kaniewski <maciejkaniewski1999@gmail.com>
2022-08-31 21:12:17 +0200

    Implement the functionality of the buttons

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # This Python file uses the following encoding: utf-8
 import sys
+import os
 from PySide6.QtGui import *
 from PySide6.QtWidgets import *
 from PySide6.QtCore import *
@@ -19,9 +20,18 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
         self.setWindowFlags(Qt.WindowCloseButtonHint | Qt.WindowMinimizeButtonHint)
 
+        self.data_collector = DataCollector()
+        self.data_collector.configure_MediaPipe_Hands(False, 1, 0.8, 0.6)  # Configure MediaPipe model
+        self.gesture_label = "0"
+
         self.CameraThread = CameraThread()
         self.CameraThread.start()
         self.CameraThread.image_update_signal.connect(self.image_update_slot)
+        self.CameraThread.landmarks_update_signal.connect(self.landmarks_update_slot)
+
+        self.ui.QPushButton_Save_Gesture.clicked.connect(self.save_gesture)
+        self.ui.QPushButton_Clear_All_Gestures.clicked.connect(self.clear_all_gestures)
+        self.ui.QSpinBox_Gesture_Label.valueChanged.connect(self.set_gesture_label)
 
     def closeEvent(self, event):
         self.CameraThread.stop()
@@ -29,15 +39,58 @@ class MainWindow(QMainWindow):
         if self.CameraThread.isFinished():
             event.accept()
 
-    def image_update_slot(self, Image):
-        self.ui.QLabel_Camera_Feed.setPixmap(QPixmap.fromImage(Image))
+    def image_update_slot(self, image):
+        self.ui.QLabel_Camera_Feed.setPixmap(QPixmap.fromImage(image))
+
+    def landmarks_update_slot(self, landmarks_list):
+        self.data_collector.multi_hand_landmarks = landmarks_list
+
+    def add_to_logs(self, message):
+        current_time = QDateTime.currentDateTime().toString("hh:mm:ss")
+        self.ui.QTextBrowser_Logs.append("[ " + current_time + " ]" + " :  " + message)
+
+    def set_gesture_label(self):
+        self.gesture_label = str(self.ui.QSpinBox_Gesture_Label.value())
+
+    def save_gesture(self):
+
+        if len(self.data_collector.multi_hand_landmarks):
+
+            self.data_collector.convert_coordinates()  # Convert coordinates to wrist-relative
+            self.data_collector.maximum_absolute_scaling()  # Perform data normalization methods
+            self.data_collector.min_max_scaling()
+            self.data_collector.standardization()
+            self.data_collector.robust_scaling()
+            self.data_collector.append_labels(self.gesture_label)
+            self.data_collector.save_data(
+            'data_collector/data/01_data_raw.csv',
+            'data_collector/data/02_data_max_abs.csv',
+            'data_collector/data/03_data_min_max.csv',
+            'data_collector/data/04_data_standardized.csv',
+            'data_collector/data/05_data_robust.csv')
+
+            self.add_to_logs("Gesture with label " + self.gesture_label + " has been successfully saved")
+
+    def clear_all_gestures(self):
+
+        if os.stat('data_collector/data/01_data_raw.csv').st_size == 0:
+            self.add_to_logs("Gesture files are already empty, no need to clear")
+        else:
+            self.data_collector.clear_data('data_collector/data/01_data_raw.csv')
+            self.data_collector.clear_data('data_collector/data/02_data_max_abs.csv')
+            self.data_collector.clear_data('data_collector/data/03_data_min_max.csv')
+            self.data_collector.clear_data('data_collector/data/04_data_standardized.csv')
+            self.data_collector.clear_data('data_collector/data/05_data_robust.csv')
+
+            self.add_to_logs("All gesture files have been cleared")
+
 
 class CameraThread(QThread):
 
     image_update_signal = Signal(QImage)
+    landmarks_update_signal = Signal(list)
 
     def run(self):
-
         data_collector = DataCollector()  # Construct Data Collector instance
         data_collector.configure_camera()  # Configure Data Collector's camera
         data_collector.configure_MediaPipe_Hands(False, 1, 0.8, 0.6)  # Configure MediaPipe model
@@ -46,17 +99,16 @@ class CameraThread(QThread):
         while self.ThreadActive:
             data_collector.detect()
             self.image_update_signal.emit(data_collector.image)
+            self.landmarks_update_signal.emit(data_collector.multi_hand_landmarks)
             data_collector.multi_hand_landmarks = None  # Clear hand landmarks
         data_collector.free_camera()  # Free data_collector's resources
 
     def stop(self):
-
         self.ThreadActive = False
         self.quit()
 
 
 if __name__ == "__main__":
-
     app = QApplication(sys.argv)
     widget = MainWindow()
     widget.show()
--- ui/ui_form.py
+++ ui/ui_form.py
@@ -15,9 +15,10 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QGroupBox, QLabel,
-    QMainWindow, QMenuBar, QSizePolicy, QStatusBar,
-    QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QGroupBox,
+    QHBoxLayout, QLabel, QMainWindow, QMenuBar,
+    QPushButton, QSizePolicy, QSpinBox, QStatusBar,
+    QTextBrowser, QVBoxLayout, QWidget)
 from resources import resources_rc
 
 class Ui_MainWindow(object):
@@ -44,6 +45,133 @@ class Ui_MainWindow(object):
         self.gridLayout.setHorizontalSpacing(16)
         self.gridLayout.setVerticalSpacing(9)
         self.gridLayout.setContentsMargins(12, -1, 12, 6)
+        self.QGroupBox_1 = QGroupBox(self.centralwidget)
+        self.QGroupBox_1.setObjectName(u"QGroupBox_1")
+        self.QGroupBox_1.setMinimumSize(QSize(620, 326))
+        font = QFont()
+        font.setBold(True)
+        self.QGroupBox_1.setFont(font)
+        self.QGroupBox_1.setStyleSheet(u"QGroupBox#QGroupBox_1 {\n"
+"    background-color:  #222831;\n"
+"    border: 2px solid gray;\n"
+"	 border-color: #00ADB5;\n"
+"    border-radius: 8px;\n"
+"    margin-top: 1ex; /* leave space at the top for the title */\n"
+"	 font-size: 16px;\n"
+"}\n"
+"\n"
+"QGroupBox::title#QGroupBox_1 {\n"
+"    color:  white;\n"
+"    subcontrol-origin: margin;\n"
+"	left: 15px;\n"
+"    padding: 0px 5px 0px 5px;\n"
+"}")
+        self.gridLayout_2 = QGridLayout(self.QGroupBox_1)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.gridLayout_2.setContentsMargins(16, 16, 16, 16)
+        self.QTextBrowser_Logs = QTextBrowser(self.QGroupBox_1)
+        self.QTextBrowser_Logs.setObjectName(u"QTextBrowser_Logs")
+        self.QTextBrowser_Logs.setStyleSheet(u"QTextBrowser#QTextBrowser_Logs {\n"
+"	color:#EEEEEE;\n"
+"}")
+
+        self.gridLayout_2.addWidget(self.QTextBrowser_Logs, 0, 0, 1, 1)
+
+
+        self.gridLayout.addWidget(self.QGroupBox_1, 1, 1, 1, 1)
+
+        self.QGroupBox_2 = QGroupBox(self.centralwidget)
+        self.QGroupBox_2.setObjectName(u"QGroupBox_2")
+        self.QGroupBox_2.setMinimumSize(QSize(620, 326))
+        self.QGroupBox_2.setFont(font)
+        self.QGroupBox_2.setStyleSheet(u"QGroupBox#QGroupBox_2 {\n"
+"    background-color:  #222831;\n"
+"    border: 2px solid gray;\n"
+"	 border-color: #00ADB5;\n"
+"    border-radius: 8px;\n"
+"    margin-top: 1ex; /* leave space at the top for the title */\n"
+"	font-size: 16px;\n"
+"}\n"
+"\n"
+"QGroupBox::title#QGroupBox_2 {\n"
+"    color:  white;\n"
+"    subcontrol-origin: margin;\n"
+"	left: 15px;\n"
+"    padding: 0px 5px 0px 5px;\n"
+"}")
+        self.frame = QFrame(self.QGroupBox_2)
+        self.frame.setObjectName(u"frame")
+        self.frame.setGeometry(QRect(420, 230, 181, 81))
+        self.frame.setFrameShape(QFrame.Panel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.gridLayout_3 = QGridLayout(self.frame)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setSpacing(8)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, -1, -1, -1)
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.QPushButton_Save_Gesture = QPushButton(self.frame)
+        self.QPushButton_Save_Gesture.setObjectName(u"QPushButton_Save_Gesture")
+        font1 = QFont()
+        font1.setPointSize(11)
+        self.QPushButton_Save_Gesture.setFont(font1)
+        self.QPushButton_Save_Gesture.setStyleSheet(u"QPushButton#QPushButton_Save_Gesture {\n"
+"	color:#EEEEEE;\n"
+"}")
+
+        self.horizontalLayout.addWidget(self.QPushButton_Save_Gesture)
+
+        self.QSpinBox_Gesture_Label = QSpinBox(self.frame)
+        self.QSpinBox_Gesture_Label.setObjectName(u"QSpinBox_Gesture_Label")
+        self.QSpinBox_Gesture_Label.setStyleSheet(u"QSpinBox#QSpinBox_Gesture_Label {\n"
+"	color:#EEEEEE;\n"
+"}")
+        self.QSpinBox_Gesture_Label.setMaximum(9)
+
+        self.horizontalLayout.addWidget(self.QSpinBox_Gesture_Label)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.QPushButton_Clear_All_Gestures = QPushButton(self.frame)
+        self.QPushButton_Clear_All_Gestures.setObjectName(u"QPushButton_Clear_All_Gestures")
+        self.QPushButton_Clear_All_Gestures.setFont(font1)
+        self.QPushButton_Clear_All_Gestures.setStyleSheet(u"QPushButton#QPushButton_Clear_All_Gestures{\n"
+"	color:#EEEEEE;\n"
+"}")
+
+        self.verticalLayout.addWidget(self.QPushButton_Clear_All_Gestures)
+
+
+        self.gridLayout_3.addLayout(self.verticalLayout, 0, 0, 1, 1)
+
+
+        self.gridLayout.addWidget(self.QGroupBox_2, 0, 1, 1, 1)
+
+        self.QGroupBox_3 = QGroupBox(self.centralwidget)
+        self.QGroupBox_3.setObjectName(u"QGroupBox_3")
+        self.QGroupBox_3.setMinimumSize(QSize(620, 326))
+        self.QGroupBox_3.setFont(font)
+        self.QGroupBox_3.setStyleSheet(u"QGroupBox#QGroupBox_3 {\n"
+"    background-color:  #222831;\n"
+"    border: 2px solid gray;\n"
+"	 border-color: #00ADB5;\n"
+"    border-radius: 8px;\n"
+"    margin-top: 1ex; /* leave space at the top for the title */\n"
+"	font-size: 16px;\n"
+"}\n"
+"\n"
+"QGroupBox::title#QGroupBox_3 {\n"
+"    color:  white;\n"
+"    subcontrol-origin: margin;\n"
+"	left: 15px;\n"
+"    padding: 0px 5px 0px 5px;\n"
+"}")
+
+        self.gridLayout.addWidget(self.QGroupBox_3, 1, 0, 1, 1)
+
         self.QGroupBox_Camera_Feed = QGroupBox(self.centralwidget)
         self.QGroupBox_Camera_Feed.setObjectName(u"QGroupBox_Camera_Feed")
         sizePolicy1 = QSizePolicy(QSizePolicy.Maximum, QSizePolicy.Preferred)
@@ -51,7 +179,9 @@ class Ui_MainWindow(object):
         sizePolicy1.setVerticalStretch(0)
         sizePolicy1.setHeightForWidth(self.QGroupBox_Camera_Feed.sizePolicy().hasHeightForWidth())
         self.QGroupBox_Camera_Feed.setSizePolicy(sizePolicy1)
+        self.QGroupBox_Camera_Feed.setMinimumSize(QSize(620, 326))
         self.QGroupBox_Camera_Feed.setMaximumSize(QSize(620, 16777215))
+        self.QGroupBox_Camera_Feed.setFont(font)
         self.QGroupBox_Camera_Feed.setStyleSheet(u"QGroupBox#QGroupBox_Camera_Feed {\n"
 "    background-color:  #222831;\n"
 "    border: 2px solid gray;\n"
@@ -81,66 +211,6 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.QGroupBox_Camera_Feed, 0, 0, 1, 1)
 
-        self.QGroupBox_2 = QGroupBox(self.centralwidget)
-        self.QGroupBox_2.setObjectName(u"QGroupBox_2")
-        self.QGroupBox_2.setStyleSheet(u"QGroupBox#QGroupBox_2 {\n"
-"    background-color:  #222831;\n"
-"    border: 2px solid gray;\n"
-"	 border-color: #00ADB5;\n"
-"    border-radius: 8px;\n"
-"    margin-top: 1ex; /* leave space at the top for the title */\n"
-"	font-size: 16px;\n"
-"}\n"
-"\n"
-"QGroupBox::title#QGroupBox_2 {\n"
-"    color:  white;\n"
-"    subcontrol-origin: margin;\n"
-"	left: 15px;\n"
-"    padding: 0px 5px 0px 5px;\n"
-"}")
-
-        self.gridLayout.addWidget(self.QGroupBox_2, 0, 1, 1, 1)
-
-        self.QGroupBox_3 = QGroupBox(self.centralwidget)
-        self.QGroupBox_3.setObjectName(u"QGroupBox_3")
-        self.QGroupBox_3.setStyleSheet(u"QGroupBox#QGroupBox_3 {\n"
-"    background-color:  #222831;\n"
-"    border: 2px solid gray;\n"
-"	 border-color: #00ADB5;\n"
-"    border-radius: 8px;\n"
-"    margin-top: 1ex; /* leave space at the top for the title */\n"
-"	font-size: 16px;\n"
-"}\n"
-"\n"
-"QGroupBox::title#QGroupBox_3 {\n"
-"    color:  white;\n"
-"    subcontrol-origin: margin;\n"
-"	left: 15px;\n"
-"    padding: 0px 5px 0px 5px;\n"
-"}")
-
-        self.gridLayout.addWidget(self.QGroupBox_3, 1, 0, 1, 1)
-
-        self.QGroupBox_1 = QGroupBox(self.centralwidget)
-        self.QGroupBox_1.setObjectName(u"QGroupBox_1")
-        self.QGroupBox_1.setStyleSheet(u"QGroupBox#QGroupBox_1 {\n"
-"    background-color:  #222831;\n"
-"    border: 2px solid gray;\n"
-"	 border-color: #00ADB5;\n"
-"    border-radius: 8px;\n"
-"    margin-top: 1ex; /* leave space at the top for the title */\n"
-"	font-size: 16px;\n"
-"}\n"
-"\n"
-"QGroupBox::title#QGroupBox_1 {\n"
-"    color:  white;\n"
-"    subcontrol-origin: margin;\n"
-"	left: 15px;\n"
-"    padding: 0px 5px 0px 5px;\n"
-"}")
-
-        self.gridLayout.addWidget(self.QGroupBox_1, 1, 1, 1, 1)
-
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
@@ -157,10 +227,12 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Drone Control Application", None))
+        self.QGroupBox_1.setTitle(QCoreApplication.translate("MainWindow", u"Logs", None))
+        self.QGroupBox_2.setTitle(QCoreApplication.translate("MainWindow", u"Utilities and settings", None))
+        self.QPushButton_Save_Gesture.setText(QCoreApplication.translate("MainWindow", u"Save gesture", None))
+        self.QPushButton_Clear_All_Gestures.setText(QCoreApplication.translate("MainWindow", u"Clear all gestures", None))
+        self.QGroupBox_3.setTitle(QCoreApplication.translate("MainWindow", u"Controls", None))
         self.QGroupBox_Camera_Feed.setTitle(QCoreApplication.translate("MainWindow", u"Camera Feed", None))
         self.QLabel_Camera_Feed.setText("")
-        self.QGroupBox_2.setTitle(QCoreApplication.translate("MainWindow", u"Settings", None))
-        self.QGroupBox_3.setTitle(QCoreApplication.translate("MainWindow", u"Controls", None))
-        self.QGroupBox_1.setTitle(QCoreApplication.translate("MainWindow", u"Logs", None))
     # retranslateUi
 
